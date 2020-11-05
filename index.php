4d09ea281491076d25211b168e7a2bff8cc8218b
429273-3 <429273-3>
2020-11-05 09:39:50 +0300

    lab2

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
-<?php
-
-class A
+<?php 
+Class A
 {
-}
-class B extends A
-{
-	public function _construct ($a1, $a2, $a3, $a4)
+	public function solve($a, $b)
 	{
-		$this->a1=$a1;
-		$this->a2=$a2;
-		$this->a3=$a3;
-		$this->a4=$a4;
+		if($a==0)
+		{
+			return null;
+		}
+	$x=-($b/(2*$a));
+	return $x;
 	}
-	protected $a1, $a2, $a3, $a4;
 }
-$a1=new A();
-$a2=new A();
-$a3=new A();
-$a4=new A();
-$b= new B($a1, $a2, $a3, $a4);
+Class B extends A
+{
+	protected function dis($a, $b, $c)
+	{
+		$d=($b*$b-(4*$a*$c));
+		return $d;
+	}
+	public function korni($a, $b, $c)
+	{
+		if ($a==0) 
+		{
+			return $this->solve($b,$c)
+		}
+		$d=$this->dis($a, $b, $c);
+		if($d<0)
+		{
+			return null;
+		}
+		if($d==0)
+		{
+			$x=(-($b)/(2*$a));
+			return $x;
+		}
+		$a=(((-$b)+sqrt($d))/(2*$a));
+		$b=(((-$b)-sqrt($d))/(2*$a));
+		return array($a, $b);
+	}
+}
+$a=new A();
+$b=new B();
+var_dump($b->korni(2,1,0));
+?>
