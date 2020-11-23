3cbeb1e091984bc19d60279aad48948e42cbbb66
nadeaboog <nadyabogdanova2001@yandex.ru>
2020-11-23 21:10:34 +0300

    9lab

--- Bogdanova/MyLog.php
+++ Bogdanova/MyLog.php
@@ -2,22 +2,37 @@
 
 use core\LogAbstract as LogAbstract;
 use core\LogInterface as LogInterface;
+use DateTime;
 
-Class MyLog extends LogAbstract implements LogInterface {
-    public function _log($str){
-        $this->log[]=$str;
-    }
-    public static function log($str){
-        self::Instance()->_log($str);
-    }
-    public function _write(){
-        foreach($this->log as $value){
-            echo $value."\n";
+Class MyLog extends LogAbstract implements LogInterface{
+        public function _write()
+        {
+            $d = new DateTime();
+            $filename = "log/".$d->format('d.m.Y_T_H.i.s.u').".log";
+            $dirname = "log";
+            if(!(is_dir($dirname))){
+                mkdir($dirname, 0777, true);
+            }
+            $logfile = "";
+            foreach($this->log as $s){
+                echo $s."\n";
+                echo $value."\r\n";
+                $logfile .= $value."\r\n";
+            }
+            file_put_contents($filename, $logfile, FILE_APPEND);
         }
-
-    }
-    public static function write(){
-        self::Instance()->_write();
+        public static function log($str)
+        {
+            MyLog::Instance()->_log($str);
+        }
+        public static function write()
+        {
+            MyLog::Instance()->_write();
+        }
+        public function _log($str)
+        {
+           $this->log[]=$str;
+        }
     }
 }
 ?>
--- index.php
+++ index.php
@@ -11,29 +11,13 @@ include "Bogdanova/MyLog.php";
 include "Bogdanova/Equation.php";
 include "Bogdanova/QuEquation.php";
 include  "Bogdanova/BogdanovaException.php";
-//
+
 ini_set("display_errors", 1);
 error_reporting (-1);
 
 try{
+    $version = file_get_context("version.txt");
+    MyLog::log("Версия программы ".$version);
+
     $b = new QuEquation();
     $values = array();
-
-    for($i=1; $i<4; $i++){
-        echo "Введите ".$i." аргумент: ";
-        $values[]=readline();
-    }
-    $va = $values[0];
-    $vb = $values[1];
-    $vc = $values[2];
-
-    MyLog::log("Введено уравнение ".$va."x^2 + ".$vb."x + ".$vc." = 0");
-    $x = $b->qu_solve($va,$vb,$vc);
-
-    $str = implode(", ", $x);
-    MyLog::log("Корни уравнения: ".$str);
-}catch(BogdanovaException $e){
-    MyLog::log($e->getMessage());
-}
-MyLog::write();
-?>
